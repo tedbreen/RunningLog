c5498c66fd1bf27ea96c7dd2cfa901629545b466
Edward Breen <ted.breen@gmail.com>
2014-05-27 01:53:23 -0700

    add avatar to user model

--- app/assets/javascripts/models/run.js
+++ app/assets/javascripts/models/run.js
@@ -27,6 +27,23 @@ window.Strava.Models.Run = Backbone.Model.extend({
       this._users = new Strava.Collections.Users();
     }
     return this._users;
+  },
+
+  displayDate: function () {
+    var date = this.get( 'start_date' );
+    var javaDate = new Date(date);
+
+    var months = ["January", "February", "March", "April",
+                  "May", "June", "July", "August",
+                  "September", "October", "November", "December"];
+
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday",
+                "Thursday", "Friday", "Saturday"]
+
+    return (days[javaDate.getDay()] + ", " +
+            months[javaDate.getMonth()] + " " +
+            javaDate.getDate() + ", " +
+            javaDate.getFullYear() );
   }
 
 });
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -27,6 +27,7 @@ class Api::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     render :json => @user
+    # render :show
   end
 
   private
